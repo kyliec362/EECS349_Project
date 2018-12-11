e2adb3dcdcb95d4b8716d4a129d7eda3843405d4
remyafong <remyafong@Remys-MacBook-Pro.local>
2018-12-10 18:54:45 -0600

    added kfold

--- trackFeatures.py
+++ trackFeatures.py
@@ -5,9 +5,11 @@ import pandas as pd
 import flask
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report, confusion_matrix
 import csv
 import random
+from sklearn.utils import shuffle
 
 username = "kchesner98"
 clientID = "1bb4ca2adbe14ec299ae000ec92fd78e"
@@ -22,22 +24,40 @@ token = util.prompt_for_user_token(username,scope = SCOPE,client_id=clientID,cli
 sp = spotipy.Spotify(auth=token)
 
 # hardcoded identifiers for different genre playlists
-countryPlaylist = "spotify:playlist:6nU0t33tQA2i0qTI5HiyRV"
-popPlaylist = "spotify:playlist:5uInm2hE4YeoFDPENhLWS9"
-pop2Playlist = "spotify:playlist:4vnp557oBcIpJ6HSv5IGPK"
-rockPlaylist = "spotify:playlist:29RO0worSJjuu77SI7GXoR"
-jazzPlaylist = "spotify:playlist:79DVeXaeZvwfrppldclP7T"
-hipHopPlaylist = "spotify:playlist:4ykrONBhOgPr4sMhAVCoPx"
-rapPlaylist = "spotify:playlist:1HDS5MxpQEMvZVBl2QAqzC"
-edmPlaylist = "spotify:playlist:37i9dQZF1DX0hvSv9Rf41p"
-edm2Playlist = "spotify:playlist:37i9dQZF1DX4dyzvuaRJ0n"
-edm3Playlist = "spotify:playlist:37i9dQZF1DXaXB8fQg7xif"
-edm4Playlist = "spotify:playlist:37i9dQZF1DX6VdMW310YC7"
-edm5Playlist = "spotify:playlist:2wxTE8gZCjfBgDHm4ljpaL"
-rnbPlaylist = "spotify:playlist:37i9dQZF1DX4SBhb3fqCJd"
+# country
+c1 = "spotify:playlist:6nU0t33tQA2i0qTI5HiyRV"
+c2 = "spotify:playlist:37i9dQZF1DWTkxQvqMy4WW"
+c3 = "spotify:playlist:37i9dQZF1DXaiEFNvQPZrM"
+# pop
+p1 = "spotify:playlist:5uInm2hE4YeoFDPENhLWS9"
+p2 = "spotify:playlist:4vnp557oBcIpJ6HSv5IGPK"
+# rock
+r1 = "spotify:playlist:29RO0worSJjuu77SI7GXoR"
+r2 = "spotify:playlist:37i9dQZF1DWXRqgorJj26U"
+# jazz
+j1 = "spotify:playlist:79DVeXaeZvwfrppldclP7T"
+j2 = "spotify:playlist:37i9dQZF1DXbITWG1ZJKYt"
+j3 = "spotify:playlist:37i9dQZF1DWTR4ZOXTfd9K"
+j4 = "spotify:playlist:37i9dQZF1DX2vYju3i0lNX"
+# hip-hop/rap
+h1 = "spotify:playlist:4ykrONBhOgPr4sMhAVCoPx"
+h2 = "spotify:playlist:1HDS5MxpQEMvZVBl2QAqzC"
+h3 = "spotify:playlist:37i9dQZF1DWY4xHQp97fN6"
+h4 = "spotify:playlist:37i9dQZF1DX0XUsuxWHRQd"
+# edm
+e1 = "spotify:playlist:37i9dQZF1DX0hvSv9Rf41p"
+e2 = "spotify:playlist:37i9dQZF1DX4dyzvuaRJ0n"
+e3 = "spotify:playlist:37i9dQZF1DXaXB8fQg7xif"
+e4 = "spotify:playlist:37i9dQZF1DX6VdMW310YC7"
+e5 = "spotify:playlist:2wxTE8gZCjfBgDHm4ljpaL"
+# r&b
+b1 = "spotify:playlist:37i9dQZF1DX4SBhb3fqCJd"
+b2 = "spotify:playlist:37i9dQZF1DWYmmr74INQlb"
+b3 = "spotify:playlist:37i9dQZF1DX6VDO8a6cQME"
+b4 = "spotify:playlist:37i9dQZF1DWXnexX7CktaI"
 
 #lists of useful information for collecting data
-listOfPlaylistIDs = [countryPlaylist,popPlaylist,rockPlaylist,jazzPlaylist, pop2Playlist, hipHopPlaylist,rapPlaylist,rnbPlaylist,edm3Playlist, edm4Playlist, edm5Playlist, edmPlaylist,edm2Playlist]
+listOfPlaylistIDs = [c1,c2,c3,p1,p2,j1,j2,j3,j4,h1,h2,h3,h4,e1,e2,e3,e4,e5,b1,b2,b3,b4]
 listOfFeatureKeys = ["id","danceability","energy","key","loudness","mode","speechiness","acousticness","instrumentalness","liveness","valence","tempo","duration_ms","time_signature"]
 listOfTimbreKeys = ["t1","t2","t3","t4","t5","t6","t7","t8","t9","t10","t11","t12"]
 listOfPitchKeys = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12"]
@@ -45,7 +65,6 @@ titleKeys = ["title"]
 
 #listOfFeatureKeys += listOfTimbreKeys
 listOfTracks = []  # list of track data
-
 
 def getTrackFeatures(trackID,playlistID):
     listOfTrackFeatures = []
@@ -66,25 +85,20 @@ def getTitle(trackID):
         return [""]
 
 def getGenre(playlist):
-    if playlist == countryPlaylist:
+    if playlist == c1 or playlist == c2 or playlist == c3:
         return "country"
-    if playlist == popPlaylist:
+    if playlist == p1 or playlist == p2:
         return "pop"
-    if playlist == rockPlaylist:
+    if playlist == r1 or playlist == r2:
         return "rock"
-    if playlist == pop2Playlist:
-        return "pop"
-    if playlist == jazzPlaylist:
+    if playlist == j1 or playlist == j2 or playlist == j3 or playlist == j4:
         return "jazz"
-    if playlist == hipHopPlaylist:
-        return "rap"
-    if playlist == rapPlaylist:
-        return "rap"
-    if playlist == rnbPlaylist:
-        return "rnb"
-    # if playlist == edm2Playlist or playlist == edmPlaylist:
-    else:
+    if playlist == h1 or playlist == h2 or playlist == h3 or playlist == h4:
+        return "hip-hop/rap"
+    if playlist == (e1 or e2 or e3 or e4 or e5):
         return "edm"
+    if playlist == b1 or playlist == b2 or playlist == b3 or playlist == b4:
+        return "rnb"
 
 def getTimbrePitch(trackId):
     audioAnalysis = sp.audio_analysis(trackId)
@@ -118,8 +132,6 @@ def getPitch(trackId):
         pitchVector[i] /= len(audioSegments)
     return pitchVector
 
-
-
 def iteratePlaylists():
     for playlistID in listOfPlaylistIDs:
         print(78,playlistID)
@@ -135,9 +147,51 @@ def iteratePlaylists():
             continue
     listOfTracks.insert(0,listOfFeatureKeys + listOfTimbreKeys + listOfPitchKeys + titleKeys + ["Genre Class"])
     df = pd.DataFrame(listOfTracks)
-    df.to_csv('trainingWithTimbrePitch.csv', index=False, header=False)
+    df.to_csv('test.csv', index=False, header=False)
 
+def train_with_kFold():
+    from copy import deepcopy
+    plotPointsCount = []
+    plotPointsPrecision = []
+    dataset = pd.read_csv("final_noid.csv", names=(listOfFeatureKeys[1:] + listOfTimbreKeys + listOfPitchKeys + ["Genre Class"]))
+    dataset = shuffle(dataset)
+    datasetSize = len(dataset)-1
+    i = 1
+    while i < datasetSize-1:
+        datasetSub = deepcopy(dataset)
+        x = pd.DataFrame()
+        y = pd.DataFrame()
+        for j in range(i):
+            ind = random.randint(1,len(datasetSub)-1)
+            datasetSub = datasetSub.drop(datasetSub.index[ind])
+            x = datasetSub.iloc[:, :-2].values  # attribute values
+            y = datasetSub.iloc[:, (len(listOfFeatureKeys) + len(listOfTimbreKeys) + len(listOfPitchKeys)) - 1].values  # genre class
+        plotPointsCount.append(datasetSize-i)
+        plotPointsPrecision.append(kFold(x,y))
+        i+=25
 
+    import matplotlib.pyplot as plt
+    plt.scatter(plotPointsCount,plotPointsPrecision)
+    plt.title('Accuracy vs. Dataset Size')
+    plt.xlabel('Dataset Size')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+def kFold(X,y):
+    from sklearn.ensemble import RandomForestClassifier
+    rfc_model = RandomForestClassifier()
+    print(145, len(X), len(y))
+    kf = KFold(n_splits=10, shuffle=True)
+    for train_index, test_index in kf.split(X,y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+    rfc_model.fit(X_train, y_train)
+    rfc_predictions = rfc_model.predict(X_test)
+    accuracy = 0
+    for i in range(len(y_test)):
+        if y_test[i] == rfc_predictions[i]:
+            accuracy += 1
+    return accuracy / len(y_test)
 
 def randomForestTraining(x,y):
     from sklearn.ensemble import RandomForestClassifier
@@ -185,7 +239,7 @@ def train_rf_with_subset():
     plt.show()
 
 #iteratePlaylists()
-train_rf_with_subset()
+#train_rf_with_subset()
+train_with_kFold()
 #randomForestTraining()
-
 
